86066c442b7570533846a407007809ee557b3e02
LihuisiEd <eduardo.lihuisi@tecsup.edu.pe>
2022-05-01 10:04:00 -0500

    Se agrega el proyecto

---
+++ db_conn.php
@@ -0,0 +1,13 @@
+<?php
+	$servidor="localhost";
+	$usuario="root";
+	$clave="";
+	$baseDedatos="lab-06";
+
+	$enlace = mysqli_connect($servidor,$usuario,$clave,$baseDedatos);
+
+	if(!$enlace){
+		echo"Error en la conexion con el servidor";
+	}
+	mysqli_set_charset($enlace,"utf8");
+?>
